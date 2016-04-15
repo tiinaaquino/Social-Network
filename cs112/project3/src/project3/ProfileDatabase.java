92817bbae72d8121f704e3c3d5a6aa4872cddb55
cbaquino <cbaquino@ebce0d44-4c41-4e8f-97e2-159a21da1814>
2016-04-15 16:34:45 +0000

    implemented starter code

---
+++ cs112/project3/src/project3/ProfileDatabase.java
@@ -0,0 +1,18 @@
+package project3;
+
+/*
+ * File: ProfileDatabase.java
+ * -------------------------------
+ * This class represents the social network: it keeps track of the profiles 
+ * of all users (and organizations if you implement the extra credit).
+ */
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class ProfileDatabase {
+
+}
